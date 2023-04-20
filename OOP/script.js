f307b281a467e79fbe34e234f9a230ac5d4194d7
Muskan Mittal <mittalmuskan758@gmail.com>
2023-04-20 12:07:35 +0530

    added style on the pages and updated OOP source code

--- OOP/script.js
+++ OOP/script.js
@@ -2,8 +2,13 @@
 
 const Person = function(firstName, birthYear) {
     // console.log(this);
+    // Instance properties
     this.firstName = firstName;
     this.birthYear = birthYear;
+
+    // this.calcAge = function(){
+    //     console.log(2037 - this.birthYear);
+    // }
 }
 
 // new Person('Jonas', 1991);
@@ -14,3 +19,21 @@ console.log(jonas);
 //2. function is called, this = {}
 //3. {} linked to prototype
 //4. function automatically return {}
+
+const matilda = new Person('Matilda', 1992);
+console.log(matilda);
+// using an operator
+console.log(jonas instanceof Person); // will return true
+const jay = "Jay";
+console.log(jay instanceof Person);  // will return false
+
+// Prototypes 
+// each and every function in javascript automatically has a property called prototype that includes constructor functions.
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+}
+
+jonas.calcAge();
+matilda.calcAge();
